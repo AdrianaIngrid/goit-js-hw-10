e54c26886e5b20555ede0d32872dbd1ffb266a67
AdrianaIngrid <150287447+AdrianaIngrid@users.noreply.github.com>
2024-05-24 07:39:11 +0300

    2

--- src/js/index.js
+++ src/js/index.js
@@ -5,46 +5,47 @@ import 'slim-select/dist/slimselect.css';
 const breedSelectElm = document.querySelector('.breed-select');
 const loaderElm = document.querySelector('.loader');
 const catInfoElm = document.querySelector('.cat-info');
-
 function createBreedOptions() {
-    breedSelectElm.classList.add('.is-hidden');
-    fetchBreeds()
-        .then(data => {
-            let optionELm = data.map(({ id, name }) => {
-                return `<option value = "${id}">${name}</option>`;
-            });
-            breedSelectElm.insertAdjacentHTML('beforeend', optionELm);
-            new SlimSelect({
-                select: '.breed-select',
-            });
-            breedSelectElm.classList.remove('.is-hidden');
-            loaderElm.classList.add('.is-hidden');
-        })
-        .catch(onError);
+  breedSelectElm.classList.add('is-hidden');
+  fetchBreeds()
+    .then(data => {
+      const optionELm = data
+        .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+        .join('');
+      breedSelectElm.insertAdjacentHTML('beforeend', optionELm);
+      new SlimSelect({
+        select: '.breed-select',
+      });
+      breedSelectElm.classList.remove('is-hidden');
+      loaderElm.classList.add('is-hidden');
+    })
+    .catch(onError);
 }
 function createBreedMarkUp(e) {
-    loaderElm.classList.replace('is-hidden', 'loader');
-    catInfoElm.classList.add('is-hidden');
-    fetchCatByBreed(e.target.value)
-        .then(data => {
-            const { url, breeds } = data[0];
-            const { name, description, temperament } = breeds[0];
-            catInfoElm.innerHTML = `<img src = "${url}" alt = "${name}"/>
-             <div class = "box">
-             <h2>${name}</h2>
-             <p>${description}</p>
-             <p><span>Temperament:</span>${temperament}</p>
-                </div>`;
-            loaderElm.classList.replace('loader', 'is-hidden');
-            catInfoElm.classList.remove('is-hidden');
-        })
-        .catch(onError);
+  loaderElm.classList.replace('is-hidden', 'loader');
+  catInfoElm.classList.add('is-hidden');
+  fetchCatByBreed(e.target.value)
+    .then(data => {
+      const { url, breeds } = data[0];
+      const { name, description, temperament } = breeds[0];
+      catInfoElm.innerHTML = `
+        <img src="${url}" alt="${name}"/>
+        <div class="box">
+          <h2>${name}</h2>
+          <p>${description}</p>
+          <p><span>Temperament:</span>${temperament}</p>
+        </div>
+      `;
+      loaderElm.classList.replace('loader', 'is-hidden');
+      catInfoElm.classList.remove('is-hidden');
+    })
+    .catch(onError);
 }
 function onError() {
-    Notiflix.Notify.failure('Oops! Ceva a mers rău! Reîncarcă pagina!');
-    breedSelectElm.classList.add('.is-hidden');
-    loaderElm.classList.add('.is-hidden');
-    catInfoElm.classList.add('.is-hidden');
+  Notiflix.Notify.failure('Oops! Ceva a mers rău! Reîncarcă pagina!');
+  breedSelectElm.classList.add('is-hidden');
+  loaderElm.classList.add('is-hidden');
+  catInfoElm.classList.add('is-hidden');
 }
 createBreedOptions();
 breedSelectElm.addEventListener('change', createBreedMarkUp);
